62e1294126d516ab39b99b8c0ff4f2a005edf754
Thomas <t.vareilhes@gmail.com>
2021-05-08 15:52:33 +0200

    models

---
+++ app/models/storage_space.rb
@@ -0,0 +1,6 @@
+class StorageSpace < ApplicationRecord
+  validates :name, :address, :city, :post_code, :country, :base_price, presence: true
+
+  has_many :bookings
+  has_many :reviews, through: :bookings
+end
---
+++ test/models/storage_space_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class StorageSpaceTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
