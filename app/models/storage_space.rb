d1f167bddea254ab9fbc5b2504969fc67945ba02
Nitin Rudrawar <nitin.rudrawar@gmail.com>
2021-05-20 19:42:23 +0100

    Search first pass

--- app/models/storage_space.rb
+++ app/models/storage_space.rb
@@ -9,4 +9,11 @@ class StorageSpace < ApplicationRecord
 
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
+
+  include PgSearch::Model
+  pg_search_scope :search_name_address_description,
+  against: [:name, :address, :description],
+  using: {
+    tsearch: {prefix: true}
+  }
 end
